c70308b85a4ed273d9b39ba8f61920ed3b72d2e7
Galiia-GR <hope05590@gmail.com>
2023-04-14 16:58:01 +0600

    add common count

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -11,7 +11,23 @@ const { NotImplementedError } = require("../extensions/index.js");
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {}
+function getCommonCharacterCount(s1, s2) {
+  if (s1 === "" || s2 === "") {
+    return 0;
+  }
+
+  let count = 0;
+
+  for (let i = 0; i < s1.length; i++) {
+    let e = s2.indexOf(s1[i]);
+    if (e >= 0) {
+      s2 = s2.replace(s1[i], "");
+      count++;
+    }
+  }
+  return count;
+}
+
 module.exports = {
   getCommonCharacterCount,
 };
