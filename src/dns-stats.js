5272678504fd25623c98d191fa60e439591f3448
Galiia-GR <hope05590@gmail.com>
2023-04-14 21:44:35 +0600

    add dns stats

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given an array of domains, return the object with the appearances of the DNS.
@@ -22,11 +22,27 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  const domObj = new Object();
+
+  const domainsArr = domains.map((el) =>
+    el.split(".").reverse().join(".").split(".")
+  );
+
+  domainsArr.reverse();
+
+  domObj[`.${domainsArr[0][0]}`] = domainsArr.length;
+
+  for (let i = 0; i < domainsArr.length; i++) {
+    if (domainsArr[i].length <= 2) {
+      domObj[`.${domainsArr[i].join(".")}`] = domainsArr.length;
+    } else {
+      domObj[`.${domainsArr[i].join(".")}`] = 1;
+    }
+  }
+  return domObj;
 }
 
 module.exports = {
-  getDNSStats
+  getDNSStats,
 };
