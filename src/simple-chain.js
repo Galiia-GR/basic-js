0ddc776aa06887e0aefc09a88876eccfefd2d638
Galiia-GR <hope05590@gmail.com>
2023-04-16 12:03:23 +0600

    + simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -17,13 +17,38 @@ const chainMaker = {
     } else if (value === null) {
       value = "null";
     }
-    this.chainArr.push(`(${value})`);
+
+    value = String(value);
+
+    this.chainArr.push(`( ${value} )`);
     return this;
   },
-  removeLink(position) {},
-  reverseChain() {},
+
+  removeLink(position) {
+    let index;
+
+    if (
+      typeof position != "number" ||
+      position === undefined ||
+      position <= 0 ||
+      position > this.chainArr.length
+    ) {
+      this.chainArr = [];
+      throw new Error("You can't remove incorrect link!");
+    }
+    index = this.chainArr.indexOf(`(${position})`);
+    this.chainArr.splice(-index, 1);
+    return this;
+  },
+
+  reverseChain() {
+    this.chainArr.reverse();
+    return this;
+  },
   finishChain() {
-    return this.chainArr.join("~~");
+    let end = this.chainArr.join("~~");
+    this.chainArr = [];
+    return end;
   },
 };
 
